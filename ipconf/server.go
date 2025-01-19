c57108c0a0f684f5c923f406003904c9306724a2
nia <1205367274@qq.com>
2025-01-20 03:02:40 +0800

    feat: add hertz server and test by hello

--- ipconf/server.go
+++ ipconf/server.go
@@ -1,4 +1,19 @@
 package ipconf
 
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
 func RunMain() {
+
+	h := server.Default()
+	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
+		c.JSON(consts.StatusOK, utils.H{"message": "Hello hertz!\n"})
+	})
+	h.Spin()
 }
