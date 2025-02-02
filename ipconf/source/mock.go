f3f9aad69e41d4f70309fc900de21653da550c4b
nia <1205367274@qq.com>
2025-02-02 23:04:03 +0800

    test: add the code framework of mock test for service register

---
+++ ipconf/source/mock.go
@@ -0,0 +1,17 @@
+package source
+
+import (
+	"context"
+)
+
+// testServiceRegister is a function to test the service register
+func testServiceRegister(ctx *context.Context, port, node string) {
+	go func() {
+		// ed := discovery.Endpoint{
+		// 	Ip:   "127.0.0.1",
+		// 	Port: port,
+		// }
+
+		// register the service
+	}()
+}
