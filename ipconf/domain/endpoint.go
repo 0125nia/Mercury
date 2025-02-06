9fbed223943ce00ca9b75923fa626ed057087b04
nia <1205367274@qq.com>
2025-02-07 02:39:53 +0800

    feat: enhance Endpoint and Stat structures, implement the code logic in dispatch layer

--- ipconf/domain/endpoint.go
+++ ipconf/domain/endpoint.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"sync/atomic"
+	"unsafe"
+)
+
 type Endpoint struct {
 	Ip          string `json:"ip"`
 	Port        string `json:"port"`
@@ -10,14 +15,34 @@ type Endpoint struct {
 }
 
 func NewEndpoint(ip, port string) *Endpoint {
-	return &Endpoint{
+	ed := &Endpoint{
 		Ip:   ip,
 		Port: port,
 	}
+	ed.window = newStateWindow()
+	ed.Stats = ed.window.getStat()
+
+	// Start a goroutine to listen to the statChan channel
+	go func() {
+		// Listen to the statChan channel and append the stat to the window
+		for stat := range ed.window.statChan {
+			ed.window.appendStat(stat)
+			newStat := ed.window.getStat()
+			// Safely update the ed.Stats pointer to point to newStat
+			// The goal is to ensure data consistency and security in the multithreaded environment
+			atomic.SwapPointer((*unsafe.Pointer)((unsafe.Pointer)(ed.Stats)), unsafe.Pointer(newStat))
+		}
+	}()
+
+	return ed
 }
 
 func (ed *Endpoint) CalculateScore(ctx *IpConfContext) {
-	// todo
+	// If the result is the same as last time, use the data last time and it isn't updated this time
+	if ed.Stats != nil {
+		ed.ActiveScore = ed.Stats.CalculateActiveScore()
+		ed.StaticScore = ed.Stats.CalculateStaticScore()
+	}
 }
 
 func (ed *Endpoint) UpdateStat(stat *Stat) {
--- ipconf/domain/stat.go
+++ ipconf/domain/stat.go
@@ -1,14 +1,25 @@
 package domain
 
+import "math"
+
 // Stat is a struct that represents the remaining resource indicator of the im gateway itself
 type Stat struct {
 	// The remaining value of the number of the im gateway connect num
+	// This belongs to static score
 	ConnectNum float64
 	// The remaining value of the number of the im gateway message bytes sending and receiving per second
+	// This belongs to active score
 	MessageBytes float64
 }
 
-// Clone returns a new Stat with the same value as the current Stat
+func (s *Stat) CalculateActiveScore() float64 {
+	return getGB(s.MessageBytes)
+}
+
+func (s *Stat) CalculateStaticScore() float64 {
+	return s.ConnectNum
+}
+
 func (s *Stat) Clone() *Stat {
 	newStat := &Stat{
 		MessageBytes: s.MessageBytes,
@@ -39,4 +50,14 @@ func (s *Stat) Sub(st *Stat) {
 	}
 	s.ConnectNum -= st.ConnectNum
 	s.MessageBytes -= st.MessageBytes
+}
+
+// getGB converts the byte value to GB
+func getGB(bytes float64) float64 {
+	return decimal(bytes / (1 << 30))
+}
+
+// decimal rounds the float64 value to two decimal places
+func decimal(value float64) float64 {
+	return math.Trunc(value*1e2+0.5) * 1e-2
 }
