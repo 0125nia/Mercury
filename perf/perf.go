d444d45ab1b2fbc0e5fb792d36ad6d3105e16da9
nia <1205367274@qq.com>
2025-02-23 17:38:20 +0800

    feat: implement TCP client sdk running in perf module

--- perf/perf.go
+++ perf/perf.go
@@ -1,11 +1,17 @@
 package perf
 
+import (
+	"net"
+
+	"github.com/0125nia/Mercury/common/sdk"
+)
+
 var (
 	TcpConnNum int32
 )
 
 func RunMain() {
 	for i := 0; i < int(TcpConnNum); i++ {
-		// todo run client sdk
+		sdk.NewChat(net.ParseIP("127.0.0.1"), 8900, "mercury", "12", "111")
 	}
 }
